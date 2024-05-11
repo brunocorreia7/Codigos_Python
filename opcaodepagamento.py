ac5836bee4ba275c1a03b9bfc63f01becbb39bc3
Bruno Correia <165749396+brunocorreia7@users.noreply.github.com>
2024-05-11 17:55:30 -0300

    GerenciadordePagamentos
    Esse Programa faz o calculo de pagamento de um produto, considerando o seu preço normal e a condição de pagamento.

---
+++ opcaodepagamento.py
@@ -0,0 +1,27 @@
+print(f'{"LOJAS CORREIA":=^40}')
+preço = float(input('Preço das compras: R$'))
+print('''FORMAS DE PAGAMENTO
+[1] à vista dinheiro/cheque
+[2] à vista cartão
+[3] 2x no cartão
+[4] 3x ou mais no cartão''')
+opção = int(input('Qual é a opção? '))
+if opção == 1:
+    total = preço - (preço * 10 / 100)
+elif opção == 2:
+    total = preço - (preço * 5 / 100)
+elif opção == 3:
+    total = preço
+    parcela = total / 2
+    print(f'Sua compra será parcelada em 2x de R${parcela:.2f} SEM JUROS')
+elif opção == 4:
+    total = preço + (preço * 20 / 100)
+    totparc = int(input('Quantas parcelas? '))
+    parcela = total / totparc
+    print(f'Sua compra será parcelada em {totparc}x de R${parcela:.2f} COM JUROS')
+else:
+    total = preço
+    print('OPÇÃO INVÁLIDA de pagamento. Tente novamente!')    
+print(f'Sua compra de R${preço:.2f} vai custar R${total:.2f} no final')
+
+
